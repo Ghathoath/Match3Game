e30d443a51fe7c4eab0f49482da2faf9c738c617
rancod <sinktar328@163.com>
2021-12-21 16:55:52 +0800

    增加了技能部分

--- player.py
+++ player.py
@@ -4,11 +4,11 @@ import random
 
 class Player:
     ROW_DAMAGE = 0
-    DAMAGE_TYPE = 0 # 0：物理   1：魔法
+    DAMAGE_TYPE = 0  # 0：物理   1：魔法
     MAX_MP = 0
     MAX_HP = 0
 
-    def __init__(self,player_type):
+    def __init__(self, player_type):
         self.player_type = player_type
         self.profile_img = pygame.image.load('image/profile.png')
         self.skill1 = pygame.image.load('image/skill1.png')
@@ -23,6 +23,16 @@ class Player:
                           4: self.skill4}
         self.stat = self.StatPlayer()
         self.read_player_stat()
+        self.blocks_require = {0: 0,
+                               1: 1,
+                               2: 1,
+                               3: 1,
+                               4: 1}
+        self.mp_require = {0: 0,
+                           1: 10,
+                           2: 10,
+                           3: 10,
+                           4: 10}
 
     def read_player_stat(self):
         # 从文件读入数值
@@ -40,6 +50,8 @@ class Player:
         skills = skillset_line.split(',')
         for skill in skills:
             self.stat.skillset.append(int(skill))
+        while len(self.stat.skillset) < 4:
+            self.stat.skillset.append(0)
         # 读入习得技能列表
         learned_skill_line = file.readline()
         learned_skill_line.strip()
@@ -50,53 +62,57 @@ class Player:
         self.MAX_MP = self.stat.mp
         file.close()
 
-    def move(self,field,index):
+    def move(self, field, index):
         self.RAW_DAMAGE = 0
-        self.skill_index(field,index)
+        self.skill_index(field, index)
         return self.RAW_DAMAGE, self.DAMAGE_TYPE
 
-    def get_damage(self,damage):
-        if damage[0]==0:
+    def get_damage(self, damage):
+        if damage[0] == 0:
             pass
-        elif damage[0]<=self.stat.armor :
-            self.stat.hp-=1
+        elif damage[0] <= self.stat.armor:
+            self.stat.hp -= 1
         else:
-            self.stat.hp-=damage[0]-self.stat.armor
-        if damage[1]==0:
+            self.stat.hp -= damage[0] - self.stat.armor
+        if damage[1] == 0:
             pass
-        elif damage[1]<=self.stat.magic_resist:
-            self.stat.hp-=1
+        elif damage[1] <= self.stat.magic_resist:
+            self.stat.hp -= 1
         else:
-            self.stat.hp-=damage[1]-self.stat.magic_resist
+            self.stat.hp -= damage[1] - self.stat.magic_resist
 
-    def effect(self,level,cube_type):
+    def effect(self, level, cube_type):
         if cube_type == 1:
-            return level*self.stat.str,0
+            return level * self.stat.str, 0
         elif cube_type == 2:
-            return level*self.stat.wis,1
+            return level * self.stat.wis, 1
         elif cube_type == 3:
             if self.stat.hp + level * 20 > self.MAX_HP:
                 self.stat.hp = self.MAX_HP
             else:
                 self.stat.hp += level * 20
-            return 0,0
+            return 0, 0
         elif cube_type == 4:
-            if self.stat.mp+level*20 > self.MAX_MP:
+            if self.stat.mp + level * 20 > self.MAX_MP:
                 self.stat.mp = self.MAX_MP
             else:
-                self.stat.mp+=level*20
-            return 0,0
+                self.stat.mp += level * 20
+            return 0, 0
         else:
-            return 0,0
+            return 0, 0
 
+    def skill_index(self, field, index):
+        if self.blocks_require[self.stat.skillset[index]]:
+            return self.blocks_require[self.stat.skillset[index]]
+        else:
+            self.skill_without_block(field, index)
 
+    def eliminate_blocks(self, field, index, blocks):
+        self.stat.mp -= self.mp_require[self.stat.skillset[index]]
+        if self.stat.skillset[index] == 1:
+            field[blocks[0][0]][blocks[0][1]] = 0
 
-    def skill_index(self,field,index):
-        if index == 1:
-            self.eliminate_blocks(field)
-            print('eliminate_blocks called')
-
-    def eliminate_blocks(self,field):
+    def skill_without_block(self, field, index):
         pass
 
     class StatPlayer(object):
